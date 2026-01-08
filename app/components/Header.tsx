2bddd898e596a34d9371394b1f799c9659543270
Airshock72 <gelagela2222@gmail.com>
2026-01-08 14:54:24 +0400

    improve Header navigation, button styles and responsive layout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -24,12 +24,12 @@ const Header = () => {
         <nav aria-label='Global' className='flex items-center justify-between p-6 lg:px-8'>
           <div className='flex lg:flex-1'>
             <Link href='#'>
-              <span className='sr-only'>Your Company</span>
               <Image
                 src='/modern-luxe/logo.png'
-                alt='Ymodern luxe logo'
+                alt='modern luxe logo'
                 width={250}
                 height={80}
+                className='w-50 lg:w-62.5'
               />
             </Link>
           </div>
@@ -37,10 +37,10 @@ const Header = () => {
             <button
               type='button'
               onClick={() => setMobileMenuOpen(true)}
-              className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
+              className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5'
             >
               <span className='sr-only'>Open main menu</span>
-              <Bars3Icon aria-hidden='true' className='size-6' />
+              <Bars3Icon aria-hidden='true' className='size-8 text-primary' />
             </button>
           </div>
           <PopoverGroup className='hidden lg:flex lg:gap-x-12'>
@@ -84,22 +84,26 @@ const Header = () => {
           </div>
         </nav>
       </div>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className='lg:hidden'>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className='lg:hidden'
+      >
         <div className='fixed inset-0 z-50' />
-        <DialogPanel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
+        <DialogPanel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-800 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
           <div className='flex items-center justify-between'>
-            <Link href='#' className='-m-1.5 p-1.5'>
-              <span className='sr-only'>Your Company</span>
-              <img
-                alt=''
-                src='https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600'
-                className='h-8 w-auto'
+            <Link href='#'>
+              <Image
+                src='/modern-luxe/logo.png'
+                alt='modern luxe logo'
+                width={250}
+                height={80}
               />
             </Link>
             <button
               type='button'
               onClick={() => setMobileMenuOpen(false)}
-              className='-m-2.5 rounded-md p-2.5 text-gray-700'
+              className='-m-2.5 rounded-md p-2.5 text-white'
             >
               <span className='sr-only'>Close menu</span>
               <XMarkIcon aria-hidden='true' className='size-6' />
@@ -107,29 +111,27 @@ const Header = () => {
           </div>
           <div className='mt-6 flow-root'>
             <div className='-my-6 divide-y divide-gray-500/10'>
-              <div className='space-y-2 py-6'>
+              <div className='space-y-4 py-6'>
                 <Link
                   href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50'
+                  className='-mx-1 rounded-sm block px-3 py-2 text-base/7 font-semibold text-white bg-gray-700/80 hover:bg-gray-200 hover:text-primary'
                 >Features
                 </Link>
                 <Link
                   href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50'
+                  className='-mx-1 rounded-sm block px-3 py-2 text-base/7 font-semibold text-white bg-gray-700/80 hover:bg-gray-200 hover:text-primary'
                 >Marketplace
                 </Link>
                 <Link
                   href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-gray-50'
+                  className='-mx-1 rounded-sm block px-3 py-2 text-base/7 font-semibold text-white bg-gray-700/80 hover:bg-gray-200 hover:text-primary'
                 >Company
                 </Link>
               </div>
               <div className='py-6'>
-                <Link
-                  href='#'
-                  className='-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-gray-50'
-                >Log in
-                </Link>
+                <Button variant='default'>
+                  <ShoppingBasket size={30} /> Shop Now
+                </Button>
               </div>
             </div>
           </div>
