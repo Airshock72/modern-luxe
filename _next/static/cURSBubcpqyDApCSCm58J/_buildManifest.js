32caf8920962b5b9507680173892146799f00d79
Airshock72 <gelagela2222@gmail.com>
2026-01-15 11:59:27 +0400

    Updates

--- _next/static/cURSBubcpqyDApCSCm58J/_buildManifest.js
+++ _next/static/cURSBubcpqyDApCSCm58J/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/modern-luxe//_next/:path+",
-        "destination": "/modern-luxe/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
